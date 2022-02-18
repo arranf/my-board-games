93f3df04ee3ff92751f238c38debdd4906b03975
Arran France <arran@arranfrance.com>
2022-02-18 00:48:43 +0000

    update rankings

--- scripts/update-config-from-csv.js
+++ scripts/update-config-from-csv.js
@@ -1,5 +1,5 @@
 //
-// This script updates the config.json to include rank from https://rankingengine.pubmeeple.com/
+// This script updates the config.json to include ranks from https://rankingengine.pubmeeple.com/
 //
 
 import csv from "csv-parser";
@@ -12,6 +12,13 @@ function sleep(ms) {
 
 async function main() {
   let config = JSON.parse(fs.readFileSync("./config.json"));
+  let titleToIdCacheContents;
+  try {
+    titleToIdCacheContents = fs.readFileSync("./title-cache.json");
+  } catch (e) {
+    // empty catch
+  }
+  const titleToIdCache = titleToIdCacheContents ? JSON.parse(titleToIdCacheContents) : {};
 
   let data = [];
   await new Promise((fulfill) =>
@@ -25,24 +32,38 @@ async function main() {
   );
 
   for (const item of data) {
-    const url = `https://boardgamegeek.com/search/boardgame?q=${encodeURIComponent(
-      item.Item
-    )}&nosession=1&showcount=20`;
-    const { data } = await axios.get(url);
-    const id = data.items[0].id;
-    console.log(`Requesting ${item.Item}`);
-    const configItem = config.additional[id.toString()];
+    const name = item.Item;
+    let id;
+    if (titleToIdCache[name]) {
+      id = titleToIdCache[name];
+    } else {
+      const url = `https://boardgamegeek.com/search/boardgame?q=${encodeURIComponent(
+        name
+      )}&nosession=1&showcount=20`;
+      const { data } = await axios.get(url);
+      const gameId = data.items[0].id;
+      console.log(`Requesting ${item.Item}`);
+      const idString = gameId.toString();
+      titleToIdCache[name] = idString;
+      id = idString;
+      console.log('Sleeping...')
+      await sleep(2000);
+    }
+
+    const configItem = config.additional[id];
 
     if (configItem) {
-      config.additional[id.toString()] = {
+      config.additional[id] = {
         ...configItem,
+        name,
         rank: item.Rank,
       };
     }
-    await sleep(2000);
   }
 
-  fs.writeFileSync("./config.json", JSON.stringify(config));
+  fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
+
+  fs.writeFileSync("./title-cache.json", JSON.stringify(titleToIdCache));
 }
 
 main();

23e970f75f81fb91563e90e256a4b482e8c5cfe0
Arran France <arran@arranfrance.com>
2022-08-30 22:24:47 +0100

    fix poor truncated expansion names

--- scripts/mybgg/indexer.py
+++ scripts/mybgg/indexer.py
@@ -1,3 +1,4 @@
+from cmath import exp
 import io
 import re
 import time
@@ -151,11 +152,19 @@ class Indexer:
             if text.startswith(prefix):
                 return text[len(prefix):]
 
+        tweaked_expansion_name = expansion_name
+
+        # Expansion name: Long Shot: The Dice Game – Horse Set 4 Mini-Expansion
+        # Game name: Long Shot: The Dice Game 
+        # --> Horse Set 4 Mini-Expansion
+        if game_name + " – " in expansion_name:
+            tweaked_expansion_name = remove_prefix(expansion_name, game_name + " - ")
+
         # Expansion name: Catan: Cities & Knights
         # Game name: Catan
         # --> Cities & Knights
-        if game_name + ": " in expansion_name:
-            return remove_prefix(expansion_name, game_name + ": ")
+        elif game_name + ": " in expansion_name:
+            tweaked_expansion_name = remove_prefix(expansion_name, game_name + ": ")
 
         # Expansion name: Shadows of Brimstone: Outlaw Promo Cards
         # Game name: Shadows of Brimstone: City of the Ancients
@@ -163,9 +172,23 @@ class Indexer:
         elif ":" in game_name:
             game_name_prefix = game_name[0:game_name.index(":")]
             if game_name_prefix + ": " in expansion_name:
-                return expansion_name.replace(game_name_prefix + ": ", "")
+                tweaked_expansion_name.replace(game_name_prefix + ": ", "")
+        
+        # Netrunner
+        if " (fan expansion for Android: Netrunner)" in tweaked_expansion_name:
+            tweaked_expansion_name.replace(" (fan expansion for Android: Netrunner)", "")
+        elif " (Fan expansion for Android: Netrunner)" in tweaked_expansion_name:
+            tweaked_expansion_name.replace(" (Fan expansion for Android: Netrunner)", "")
 
-        return expansion_name
+        # Mini-Expansion
+        if "Mini-Expansion" in tweaked_expansion_name:
+            tweaked_expansion_name.replace("Mini-Expansion", "")
+        elif "Mini Expansion" in tweaked_expansion_name:
+            tweaked_expansion_name.replace("Mini Expansion", "")
+        elif tweaked_expansion_name.endswith("Expansion"):
+            tweaked_expansion_name.replace("Expansion", "")
+
+        return tweaked_expansion_name.strip()
 
     def fetch_image(self, url, tries=0):
         try:

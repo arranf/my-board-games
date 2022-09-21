ee134079522b434e0a5cedbb937470f95cd6a2a4
Arran France <arran@arranfrance.com>
2022-09-21 21:17:05 +0100

    Actually get list count python

--- scripts/mybgg/indexer.py
+++ scripts/mybgg/indexer.py
@@ -239,6 +239,6 @@ class Indexer:
         docs = self.index.get_documents({'limit':1000, 'fields': 'id'})
         collection_ids = [x.id for x in collection]
         ids_to_delete = [x["id"] for x in docs["results"] if x["id"] not in collection_ids]
-        if ids_to_delete.count() > 0:
+        if len(ids_to_delete) > 0:
             print("Ids to delete", ids_to_delete)
             self.index.delete_documents(ids_to_delete)
